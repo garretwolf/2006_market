bae88c9feb16f3b8d9a1315056c8544f8b4d3045
Garret Wolf <garretwolf26@gmail.com>
2020-07-29 11:38:05 -0400

    Refactor test_it_can_stock_and_check_stock to follow TDD

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -20,28 +20,37 @@ class VendorTest < Minitest::Test
     assert_equal expected, vendor.inventory
   end
 
-  def test_it_can_stock_inventory
+  def test_it_can_stock_and_check_stock
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    vendor.stock(item1, 30)
-    vendor.stock(item1, 25)
-    vendor.check_stock(item1)
+    # assert_equal 0, vendor.check_stock(item1)
 
+    vendor.stock(item1, 30)
+    expected = {item1 => 30}
+    assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+
+    vendor.stock(item1, 25)
     assert_equal 55, vendor.check_stock(item1)
-  end
 
-  def test_it_can_return_inventory
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    vendor.stock(item1, 30)
-    vendor.stock(item1, 25)
     vendor.stock(item2, 12)
-
     expected = {item1 => 55, item2 => 12}
-
     assert_equal expected, vendor.inventory
   end
+
+  # def test_it_can_return_inventory
+  #   skip
+  #   vendor = Vendor.new("Rocky Mountain Fresh")
+  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
+  #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  #   vendor.stock(item1, 30)
+  #   vendor.stock(item1, 25)
+  #   vendor.stock(item2, 12)
+  #
+  #   expected = {item1 => 55, item2 => 12}
+  #
+  #   assert_equal expected, vendor.inventory
+  # end
 
 end
