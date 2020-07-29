4c75324eda6a1d12aea91247a8965bea6574bf8c
Garret Wolf <garretwolf26@gmail.com>
2020-07-29 11:39:58 -0400

    Remove commented out test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -39,18 +39,4 @@ class VendorTest < Minitest::Test
     assert_equal expected, vendor.inventory
   end
 
-  # def test_it_can_return_inventory
-  #   skip
-  #   vendor = Vendor.new("Rocky Mountain Fresh")
-  #   item1 = Item.new({name: 'Peach', price: "$0.75"})
-  #   item2 = Item.new({name: 'Tomato', price: '$0.50'})
-  #   vendor.stock(item1, 30)
-  #   vendor.stock(item1, 25)
-  #   vendor.stock(item2, 12)
-  #
-  #   expected = {item1 => 55, item2 => 12}
-  #
-  #   assert_equal expected, vendor.inventory
-  # end
-
 end
